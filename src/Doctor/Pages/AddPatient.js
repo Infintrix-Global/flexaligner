e190b962e8192005970381d6ee4c9e364b50e365
janhvi1998 <97932514+janhvi1998@users.noreply.github.com>
2022-07-25 12:13:45 +0530

    Doctor

--- src/Doctor/Pages/AddPatient.js
+++ src/Doctor/Pages/AddPatient.js
@@ -7,12 +7,31 @@ import {
   Tab,
   Form,
   InputGroup,
+  Navbar,
+  Dropdown,
+  Card
 } from "react-bootstrap";
 import React, { useState } from "react";
 import { TbUser } from "react-icons/tb";
 import { FaCalendarAlt } from "react-icons/fa";
 import "../../Doctor/Styles/AddPatient.css";
+import user from "../../Assets/user.png";
+import logo from "../../Assets/Ologo.png";
+import { IoMdNotifications } from "react-icons/io";
+import { FiMessageSquare, FiPower } from "react-icons/fi";
+import { FaBars } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
 function AddPatient() {
+  const tglContent = () => {
+    let Menu = document.querySelector(".menuTab");
+
+    if (Menu.classList.contains("collapsed")) {
+      Menu.classList.remove("collapsed");
+    } else {
+      Menu.classList.add("collapsed");
+    }
+  };
+
   const [validated, setValidated] = useState(false);
 
   const handleSubmit = (event) => {
@@ -27,6 +46,78 @@ function AddPatient() {
 
   return (
     <>
+      <Navbar collapseOnSelect expand="lg" className="navb">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="" className="" width={120} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Button variant="" onClick={tglContent}>
+                <FaBars fontSize={28} color="white" />
+              </Button>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">
+                <IoMdNotifications fontSize={30} color="white" />
+              </Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <FiMessageSquare fontSize={30} color="white" className="me-2" />
+              </Nav.Link>
+              <span>
+                <img src={user} alt="" width={35} className="mt-1" />
+              </span>
+              <Nav.Link href="#deets" className="p-0 mx-2 mt-1">
+                <Dropdown>
+                  <Dropdown.Toggle
+                    variant=""
+                    id="dropdown-basic"
+                    className="user"
+                  >
+                    admin@gmail.com
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">
+                      <CgProfile fontSize={25} />
+                      <span className="px-3">Profile</span>
+                    </Dropdown.Item>
+                    <hr />
+                    <Dropdown.Item href="#/action-2">
+                      <FiPower fontSize={25} />
+                      <span className="px-3">Logout</span>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container fluid>
+            <Row className="menuTab">
+              <Col>
+              <Card body className="border-0">
+                {/* <Row>
+                  <Col>
+                  <Button variant="link" className="doc-tab">Doctor</Button>
+                  </Col>
+                  <Col>
+                  <Button variant="link" className="prof-tab">Profile</Button>
+                  
+                  </Col>
+                </Row> */}
+                 <Nav className="justify-content-center">
+                 <Nav.Link href="#deets" className="doc-tab active">Doctor</Nav.Link>
+                 <Nav.Link href="#deets" className="prof-tab">Profile</Nav.Link>
+            
+          </Nav>
+              </Card>
+              </Col>
+            </Row>
+        </Container>
+
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Container
           className="pt-5 mt-5  pb-5 mb-5"
