aff85532aad59b4a4a14bc5af5cede43b6837e57
janhvi1998 <97932514+janhvi1998@users.noreply.github.com>
2022-07-25 11:50:41 +0530

    Doctor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./Admin/Pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddDoctor from "./Admin/Pages/AddDoctor";
 import DoctorsList from "./Admin/Pages/DoctorsList";
+import AddPatient from "./Doctor/Pages/AddPatient";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route path="/" element={<Login/>}/>
           <Route path="/add-doctor" element={<AddDoctor/>}/>
           <Route path="/view-doctors" element={<DoctorsList/>}/>
-
+          <Route path="/add-patient" element={<AddPatient/>}/>
         </Routes>
       </Router>
     </div>
